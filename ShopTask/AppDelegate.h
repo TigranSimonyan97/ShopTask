d86f2e255c7f7ff664cf347e67fccbad299ba1e0
Tke <tigsim97@gmail.com>
2017-11-25 12:05:22 +0400

    Initial Commit

---
+++ ShopTask/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ShopTask
+//
+//  Created by Tiko on 11/25/17.
+//  Copyright © 2017 Tigranakert. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ShopTask/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ShopTask
+//
+//  Created by Tiko on 11/25/17.
+//  Copyright © 2017 Tigranakert. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
